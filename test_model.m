9650be0e8c17a54fdecdb7c98bac14340990c337
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2020-05-05 04:34:53 +1200

    vectorised hit rate

--- test_model.m
+++ test_model.m
@@ -11,9 +11,9 @@ likelihood_ste_male_angry(2,1:12) = naive_bayes(ave_ste_male_angry(index_male_an
 likelihood_ste_male_angry(3,1:12) = naive_bayes(ave_ste_male_angry(index_male_angry+1:end), phat_ste_male_happy  );
 likelihood_ste_male_angry(4,1:12) = naive_bayes(ave_ste_male_angry(index_male_angry+1:end), phat_ste_male_sad    );
 
-[p, idx] = max(likelihood_ste_male_angry);
+[~, idx(1,:)] = max(likelihood_ste_male_angry);
 % emotions(idx)
-hit_rate_male_angry = numel(idx( idx==1 )) / numel(idx)
+% hit_rate_male(1) = numel(idx( idx==1 )) / numel(idx)
 
 
 %==== male excited ====%
@@ -24,9 +24,9 @@ likelihood_ste_male_excited(2,1:12) = naive_bayes(ave_ste_male_excited(index_mal
 likelihood_ste_male_excited(3,1:12) = naive_bayes(ave_ste_male_excited(index_male_excited+1:end), phat_ste_male_happy  );
 likelihood_ste_male_excited(4,1:12) = naive_bayes(ave_ste_male_excited(index_male_excited+1:end), phat_ste_male_sad    );
 
-[p, idx] = max(likelihood_ste_male_excited);
+[~, idx(2,:)] = max(likelihood_ste_male_excited);
 % emotions(idx)
-hit_rate_male_excited = numel(idx( idx==2 )) / numel(idx)
+% hit_rate_male(2) = numel(idx( idx==2 )) / numel(idx)
 
 
 %==== male happy ====%
@@ -37,9 +37,9 @@ likelihood_ste_male_happy(2,1:12) = naive_bayes(ave_ste_male_happy(index_male_ha
 likelihood_ste_male_happy(3,1:12) = naive_bayes(ave_ste_male_happy(index_male_happy+1:end), phat_ste_male_happy  );
 likelihood_ste_male_happy(4,1:12) = naive_bayes(ave_ste_male_happy(index_male_happy+1:end), phat_ste_male_sad    );
 
-[p, idx] = max(likelihood_ste_male_happy);
+[~, idx(3,:)] = max(likelihood_ste_male_happy);
 % emotions(idx)
-hit_rate_male_happy = numel(idx( idx==3 )) / numel(idx)
+% hit_rate_male(3) = numel(idx( idx==3 )) / numel(idx)
 
 
 %==== male sad ====%
@@ -50,8 +50,8 @@ likelihood_ste_male_sad(2,1:12) = naive_bayes(ave_ste_male_sad(index_male_sad+1:
 likelihood_ste_male_sad(3,1:12) = naive_bayes(ave_ste_male_sad(index_male_sad+1:end), phat_ste_male_happy  );
 likelihood_ste_male_sad(4,1:12) = naive_bayes(ave_ste_male_sad(index_male_sad+1:end), phat_ste_male_sad    );
 
-[p, idx] = max(likelihood_ste_male_sad);
+[~, idx(4,:)] = max(likelihood_ste_male_sad);
 % emotions(idx)
-hit_rate_male_sad = numel(idx( idx==4 )) / numel(idx)
+% hit_rate_male(4) = numel(idx( idx==4 )) / numel(idx)
 
 
